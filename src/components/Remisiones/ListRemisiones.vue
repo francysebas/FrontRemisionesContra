2bf3ec76cdf4adb6e27248209c5889f7cbdcb2f8
francysebas <31090790+francysebas@users.noreply.github.com>
2022-05-26 17:22:17 -0500

    front remisiones

---
+++ src/components/Remisiones/ListRemisiones.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col text-left">
+                <h2>
+                    listado de remisiones
+                </h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
